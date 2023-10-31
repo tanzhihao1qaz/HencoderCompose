65df25303191e619835c2230e443db48b40e84ab
sleepingcat <1119417707@qq.com>
2023-10-31 09:04:14 +0800

    补充注释

--- ft-mine/src/main/java/com/sleepingcat/ft_mine/MineFragment.kt
+++ ft-mine/src/main/java/com/sleepingcat/ft_mine/MineFragment.kt
@@ -12,9 +12,9 @@ import com.sleepingcat.nav_plugin_runtime.NavDestination
 /**
  * @作者 志浩
  * @时间 2023/10/30 20:39
- * @描述 TODO
+ * @描述 deeplink注意事项：真机调试如果地址未注册，不能用http协议，否则打不开，但是模拟器可以；另外如果是http或https，最后要以/结尾
  */
-@NavDestination(type = NavDestination.NavType.Fragment, route = "mine", deeplink = "test://com.techme.jetpack/user?phone={phone}")
+@NavDestination(type = NavDestination.NavType.Fragment, route = "mine", deeplink = "test://com.techme.jetpack/user?phone={phone}/")
 class MineFragment : BaseFragment() {
     override val TAG = "MineFragment"
 
